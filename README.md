8bffddb8127dc97b9514897c8f4fb6466157ef78
Melvin <melvinmmelo@gmail.com>
2022-11-11 17:07:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Learning React 2022
+11-Nov-22
+
+- Study it
+- Make a template
+- Implement
---
+++ index.js
@@ -0,0 +1,10 @@
+ReactDOM.render(
+  <div>
+    <h2>Unordered List</h2>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ul>
+  </div>,
+  document.querySelector("#app")
+)
